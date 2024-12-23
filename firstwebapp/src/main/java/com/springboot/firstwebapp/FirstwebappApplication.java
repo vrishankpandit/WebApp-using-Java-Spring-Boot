0e8a28a8baad1b9d6b6d835f8dd053d0ec2b6730
Vishu <vrishankpandit1344@gmail.com>
2024-12-23 14:14:37 +0530

    test

--- firstwebapp/src/main/java/com/springboot/firstwebapp/FirstwebappApplication.java
+++ firstwebapp/src/main/java/com/springboot/firstwebapp/FirstwebappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstwebappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(FirstwebappApplication.class, args);
 	}
 
