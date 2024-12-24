8f47b919d4cca07d9bfd772c1d79143fb78db1e7
Vishu <vrishankpandit1344@gmail.com>
2024-12-25 01:33:54 +0530

    feat(Logging):add methods for logging at debug,info,warn levels

--- firstwebapp/src/main/java/com/springboot/firstwebapp/login/LoginController.java
+++ firstwebapp/src/main/java/com/springboot/firstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.springboot.firstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +10,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
+	private Logger logger=LoggerFactory.getLogger(getClass());
+	
 //	http://localhost:8081/login?name=vp
 //	Model
 	@RequestMapping("login")
 	public String GoToLoginPage(@RequestParam String name,ModelMap model) {
 //		System.out.println(name);
+		logger.debug("name is {}",name);
+		logger.info("print at info level");
+		logger.warn("print at warn level");
+		
 		model.put("name", name);
 		return "login";
 	}
