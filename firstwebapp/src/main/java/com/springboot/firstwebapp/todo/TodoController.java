245388e619237de300eeb3862da471dc3872378f
Vishu <vrishankpandit1344@gmail.com>
2024-12-27 17:06:58 +0530

    feat:add Add Todo Page on Click Submit

--- firstwebapp/src/main/java/com/springboot/firstwebapp/todo/TodoController.java
+++ firstwebapp/src/main/java/com/springboot/firstwebapp/todo/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TodoController {
@@ -24,5 +25,15 @@ public class TodoController {
 		model.addAttribute("todos", todos);  //(firstname is mapped to the attribute in html,whereas second name is local variable)
 		return "ListTodos";
 		
+	}
+	
+	@RequestMapping(value="add-todo",method=RequestMethod.GET)
+	public String showNewTodoPage() {
+		return "todo";
+	}
+	
+	@RequestMapping(value="add-todo",method=RequestMethod.POST)
+	public String addNewTodo() {
+		return "redirect:list-todos";
 	}
 }
