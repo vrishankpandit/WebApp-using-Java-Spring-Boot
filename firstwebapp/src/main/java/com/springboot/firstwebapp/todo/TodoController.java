cd7832ff1c9744a8aca1c508725c835bee652cc3
Vishu <vrishankpandit1344@gmail.com>
2024-12-27 22:31:56 +0530

    feat(Validation):add 2 way binding(Command Bean) b/w controller and jsp
    Binding username directly to Todo.java hence removing requestParam

--- firstwebapp/src/main/java/com/springboot/firstwebapp/todo/TodoController.java
+++ firstwebapp/src/main/java/com/springboot/firstwebapp/todo/TodoController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -30,14 +29,17 @@ public class TodoController {
 	}
 	
 	@RequestMapping(value="add-todo",method=RequestMethod.GET)
-	public String showNewTodoPage() {
+	public String showNewTodoPage(ModelMap model) {
+		String username=(String)model.get("name");
+		Todo todo=new Todo(0,username,"",LocalDate.now().plusYears(1),false);
+		model.put("todo", todo);
 		return "todo";
 	}
 	
 	@RequestMapping(value="add-todo",method=RequestMethod.POST)
-	public String addNewTodo(@RequestParam String description,ModelMap model) {
-		String username=(String)model.get("NAME");
-		todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+	public String addNewTodo(ModelMap model,Todo todo) {
+		String username=(String)model.get("name");
+		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
 		
 		return "redirect:list-todos";
 	}
